57fb5e04b1dc5ce74a15338949e0324b9bb692a3
Calvin Webster <calweb@gmail.com>
2016-03-08 11:33:42 -0500

    readd

---
+++ app/regex.js
@@ -0,0 +1,27 @@
+// This is a bonus section!
+exports = (typeof window === 'undefined') ? global : window;
+
+exports.regexAnswers = {
+  containsNumber : function(str) {
+
+  },
+
+  containsRepeatingLetter : function(str) {
+
+  },
+
+  endsWithVowel : function(str) {
+
+  },
+
+  captureThreeNumbers : function(str) {
+
+  },
+
+  matchesPattern : function(str) {
+
+  },
+  isUSD : function(str) {
+
+  }
+};
