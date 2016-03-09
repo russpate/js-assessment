f4f16c71f4f6ee972b31b2a6ec22e876e6785e16
Russell Pate <russ@russpate.com>
2016-03-08 21:55:59 -0500

    arrays assessment complete minus a couple

--- app/arrays.js
+++ app/arrays.js
@@ -1,57 +1,77 @@
+/* jshint ignore: start */
+
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
-
+    return _.reduce(arr, function(sum, el){
+      return sum + el
+    },0);
   },
 
   remove : function(arr, item) {
-
+    return _.without(arr, item);
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+     if (arr[i] === item) arr.splice(i, item);
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var newArr = arr1.concat(arr2);
+    return newArr;
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    var counter = [];
+    for(var i = 0; i < arr.length; ++i){
+      if(arr[i] === item)
+        counter++;
+    }
+    return counter;
   },
 
   duplicates : function(arr) {
-
+    
   },
 
   square : function(arr) {
-
+    return mapArr = _.map(arr, function(num){
+    return Math.pow(num, 2);
+    });
   },
 
   findAllOccurrences : function(arr, target) {
